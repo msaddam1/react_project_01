a1e0ae18bcd4007a561b0fb451c24ea9b9f8f756
Saddam <msaddamh007@gmail.com>
2023-09-22 13:33:47 +0600

    complete 1st react project

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -37,7 +37,7 @@ export default function TextForm(props) {
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 
-                <textarea className="form-control" value={text} style={{backgroundColor: props.mode==='dark'?'grey':'white',color: props.mode==='dark'?'white':'#042743'}} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} style={{backgroundColor: props.mode==='dark'?'#13466e':'white',color: props.mode==='dark'?'white':'#042743'}} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-1 my-1" onClick={handleLowClick}>Convert to Lowercase</button>
@@ -48,7 +48,7 @@ export default function TextForm(props) {
         <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h2>Your text summary</h2>
             <p>{text.split(' ').filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
-            <p>{text.split(' ').length *0.008 } minutes to read</p>
+            <p>{text.split(' ').filter((element)=>{return element.length!==0}).length *0.008 } minutes to read</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:"Enter something in the textbox above to preview it here "}</p>
         </div>
